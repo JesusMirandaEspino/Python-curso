0b48892656db37cdae1d988c0a7ae5cf5116feb3
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-28 09:25:44 -0600

    tipos de datos

---
+++ dia2/datos.py
@@ -0,0 +1,16 @@
+# string
+print('Texto 456')
+# integer
+print(123)
+# float
+print(3.25)
+# list
+print(['hola', 123, 'que tal', 789])
+# dic
+print({ 'nombre': 'Jesus', 'apellido': 'Miranda' })
+# tupla
+print(('hola', 123, 'que tal', 789))
+# set
+print({'a', 'b', 'c', 'd', 'e', 'f'})
+#boolean
+print(True, False)
