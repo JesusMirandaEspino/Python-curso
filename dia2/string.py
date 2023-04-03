d81ff8d5d7a375829c901f2b6aaa361421e3be7d
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-03 12:19:28 -0500

    concatenar format

---
+++ dia2/string.py
@@ -0,0 +1,10 @@
+auto = 'Bocho'
+matricula = 123435
+x = 10
+y = 15
+print('Mi auto es {} y de matricula {}'.format(auto, matricula))
+print(f'Mi auto es {auto} y de matricula {matricula}')
+
+
+print('Mi primer numero  es {} y el segundo numero es {}'.format(x, y))
+print(f'Mi primer numero es {x} y el segundo numero es {y}')
