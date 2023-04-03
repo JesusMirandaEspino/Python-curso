1960e2a0f4780f4377f572583f1a7a6f3d14da27
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-03 12:28:54 -0500

    concatenar format parte 3

--- dia2/string.py
+++ dia2/string.py
@@ -9,3 +9,14 @@ print(f'Mi auto es {auto} y de matricula {matricula}')
 print('Mi primer numero  es {} y el segundo numero es {}'.format(x, y))
 print('Mi primer numero  es {} y el segundo numero es {} la suma es {}'.format(x, y, x + y ))
 print(f'Mi primer numero es {x} y el segundo numero es {y} yla suma es { x + y}')
+
+nombre_asociado = "Jesse Pinkman"
+numero_asociado = 399058
+
+print(f'Estimado/a {nombre_asociado}, su número de asociado es: {numero_asociado}')
+
+
+puntos_nuevos = 350
+puntos_totales = 1225
+
+print(f'Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos')
