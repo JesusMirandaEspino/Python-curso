8e8e311fa7f7a67b1886451e3f154c303d181f8a
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-07 10:18:36 -0500

    index parte 3

--- dia3/index.py
+++ dia3/index.py
@@ -3,9 +3,16 @@ mi_nuevo_texto = 'esta es una prueba'
 l = mi_texto.index('o')
 resultado = mi_nuevo_texto.index('prueba')
 res_a = mi_nuevo_texto.index('a')
+res_ar = mi_nuevo_texto.rindex('a')
 res_a2 = mi_nuevo_texto.index('a',5)
+res_a3 = mi_nuevo_texto.index('a',5, 10)
 print(mi_nuevo_texto[9])
 print(l)
 print(resultado)
 print(res_a)
 print(res_a2)
+print(res_a3)
+print(res_ar)
+
+palabra = "ordenador"
+print(palabra[4])
