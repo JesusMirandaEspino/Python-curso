d8ec058b964ddd6063c2180855d5ad3b9fe564fb
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-07 10:20:38 -0500

    index parte 4

--- dia3/index.py
+++ dia3/index.py
@@ -16,3 +16,9 @@ print(res_ar)
 
 palabra = "ordenador"
 print(palabra[4])
+
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+print(frase.index('práctica'))
+
+frase = "En teoría, la teoría y la práctica son los mismos. En la práctica, no lo son."
+print(frase.rindex('práctica'))
