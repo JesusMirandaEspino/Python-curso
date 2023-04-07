d0a00c40e2b59bbb65dfd6e38feb60f81f82075c
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-07 10:16:19 -0500

    index parte 2

--- dia3/index.py
+++ dia3/index.py
@@ -1,5 +1,11 @@
 mi_texto = 'hola'
 mi_nuevo_texto = 'esta es una prueba'
 l = mi_texto.index('o')
+resultado = mi_nuevo_texto.index('prueba')
+res_a = mi_nuevo_texto.index('a')
+res_a2 = mi_nuevo_texto.index('a',5)
 print(mi_nuevo_texto[9])
 print(l)
+print(resultado)
+print(res_a)
+print(res_a2)
