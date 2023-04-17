32bb1cfa946d2a22b9cb3da5d555eda2e6e49ca6
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-17 10:29:28 -0500

    index parte 7

--- dia3/index.py
+++ dia3/index.py
@@ -42,3 +42,13 @@ print(fragmento)
 frase = "Controlar la complejidad es la esencia de la programación"
 fragmento = frase[:9]
 print(fragmento)
+
+
+frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+fragmento = frase[8::3]
+print(fragmento)
+
+
+frase = "Es genial trabajar con ordenadores. No discuten, lo recuerdan todo y no se beben tu cerveza"
+fragmento = frase[::-1]
+print(fragmento)
