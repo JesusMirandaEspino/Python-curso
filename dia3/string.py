1e5bda1039a8e59faed6ab5c56fc96501b389470
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-18 10:12:33 -0500

    strings

---
+++ dia3/string.py
@@ -0,0 +1,17 @@
+texto = 'Este es un texto'
+a = 'aprender'
+b = 'phyton'
+c = ' '.join([a,b])
+resultado = texto.upper()
+print(resultado)
+
+resultado = texto.split()
+print(resultado)
+
+resultado = texto.lower()
+print(resultado)
+
+resultado = texto.find('s')
+print(resultado)
+
+print(c)
