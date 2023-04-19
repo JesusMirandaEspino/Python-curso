dce8d9f15504de0ac9792bcdb401d8036e6b0fe7
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-19 10:34:15 -0500

    listas parte 1

---
+++ dia3/listas.py
@@ -0,0 +1,12 @@
+lista = ['a', 'b', 'c', 'd', 'e', 'f']
+otra_lista = [99, 55, 10, 56, 2]
+print(len(lista))
+print(lista[0])
+print(lista[0:2])
+print(lista + otra_lista)
+lista.append('h')
+print(lista)
+lista.pop(0)
+print(lista)
+otra_lista.sort()
+print(otra_lista)
