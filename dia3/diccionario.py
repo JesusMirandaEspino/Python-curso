118b2ef2848fef5b3f94d963ba9d863cd4ab3948
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-20 09:55:22 -0500

    diccionario parte 3

--- dia3/diccionario.py
+++ dia3/diccionario.py
@@ -8,6 +8,8 @@ nueva_lista = { 'c1': 55, 'c2': [10,20,30], 'c3': { 's1': 'a', 's2': 'c' } }
 #edad: 35
 #ocupacion: Periodista
 mi_dic = { 'nombre': 'Karen', 'apellido': 'Jurgens', 'edad': 35, 'ocupacion': 'Periodista' }
+mi_dict = {"valores_1":{"v1":3,"v2":6},"puntos":{"points1":9,"points2":[10,300,15]}}
+print(mi_dict['puntos']['points2'][1])
 consulta = (cliente['apellido'])
 print(diccionario)
 print(diccionario['c1'])
