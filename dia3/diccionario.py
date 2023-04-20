d7071dff5f0a75557f060159445ab471e36396b4
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-20 09:53:30 -0500

    diccionario parte 2

--- dia3/diccionario.py
+++ dia3/diccionario.py
@@ -3,6 +3,11 @@ diccionario['c3'] = 'valor3'
 cliente = { 'nombre': 'Jesus', 'apellido': 'Miranda', 'peso': 80, 'altura':1.76 }
 lista = { 'c1': 55, 'c2': [10,20,30], 'c3': { 's1': 100, 's2': 200 } }
 nueva_lista = { 'c1': 55, 'c2': [10,20,30], 'c3': { 's1': 'a', 's2': 'c' } }
+#nombre: Karen
+#apellido: Jurgens
+#edad: 35
+#ocupacion: Periodista
+mi_dic = { 'nombre': 'Karen', 'apellido': 'Jurgens', 'edad': 35, 'ocupacion': 'Periodista' }
 consulta = (cliente['apellido'])
 print(diccionario)
 print(diccionario['c1'])
