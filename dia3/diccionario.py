3c3d25e5e9123e78b4e0ade88817b4b1d469fc78
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-20 09:47:47 -0500

    diccionario parte 1

---
+++ dia3/diccionario.py
@@ -0,0 +1,13 @@
+diccionario = { 'c1': 'valor1', 'c2': 'valor2'}
+diccionario['c3'] = 'valor3'
+cliente = { 'nombre': 'Jesus', 'apellido': 'Miranda', 'peso': 80, 'altura':1.76 }
+lista = { 'c1': 55, 'c2': [10,20,30], 'c3': { 's1': 100, 's2': 200 } }
+nueva_lista = { 'c1': 55, 'c2': [10,20,30], 'c3': { 's1': 'a', 's2': 'c' } }
+consulta = (cliente['apellido'])
+print(diccionario)
+print(diccionario['c1'])
+print(consulta)
+print(lista['c2'][1])
+print(lista['c3']['s2'])
+print(nueva_lista['c3']['s2'].upper())
+print(lista.keys())
