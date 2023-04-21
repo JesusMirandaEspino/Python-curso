362d35ef9caafe3fc582712a06a8a07efec2dd4e
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-21 09:54:11 -0500

    set part 2

--- dia3/set.py
+++ dia3/set.py
@@ -1,6 +1,18 @@
 mi_set = set([1,2,3,4,5])
 otro_set = { 1,2,3,4,5, (1,2,3) }
+s1 = {1,2,3,4}
+s2 = {2,3,4,5,6,7}
+s3 = s1.union(s2)
+s3.add(8)
+sorteo = s3.pop()
+s3.discard(1)
 print(type(mi_set))
 print(mi_set)
 print(type(otro_set))
 print(otro_set)
+
+print(len(mi_set))
+print(2 in mi_set)
+print(s3)
+print(sorteo)
+s3.clear()
