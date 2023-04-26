8e688338f2c7caed86f3e8505a6e26c3fa643717
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-26 10:57:25 -0500

    booleanos parte 2

--- dia3/booleanos.py
+++ dia3/booleanos.py
@@ -3,6 +3,16 @@ valor2 = False
 print(type(valor1))
 print(valor1)
 
+
+
 numero = 5 > 2 + 1
 print(type(numero))
 print(numero)
+
+lista = [1,2,3,4,5]
+numero1 = 5 in lista
+numero2 = 6 not in lista
+print(type(numero1))
+print(numero1)
+print(type(numero2))
+print(numero2)
