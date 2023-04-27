d5e12540df5bac13c968790f155d3d10f9432de9
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-27 10:13:26 -0500

    procesador de texto parte 2

--- dia3/proyecto.py
+++ dia3/proyecto.py
@@ -16,3 +16,29 @@ print( f'Hemos encontrado la letra {letras[0]} repetiva {cantidad_letras1} veces
 print( f'Hemos encontrado la letra {letras[1]} repetiva {cantidad_letras2} veces' )
 print( f'Hemos encontrado la letra {letras[2]} repetiva {cantidad_letras3} veces' )
 
+print('')
+print('Cantidad de palabras')
+palabras = texto.split()
+print('Cantidad de las palabras es {len(palabras)}')
+
+print('')
+print('Letras de inicio y fin')
+letra_ini = texto[0]
+letra_f = texto[-1]
+print(f'La letra de inicio del texto es {letra_ini} y la letra final es {letra_f}')
+
+print('')
+print('Texto invertido')
+palabras.reverse()
+texto_invertido = ' '.join(palabras)
+print(f'El texto inverido es {texto_invertido}')
+
+
+print('')
+print('Buscando la palabra phyton')
+buscar = 'phyton' in texto
+dic = {True:'si', False:'no'}
+print(f'La palabra phyton {dic[buscar]} se encuentra en el texto')
+
+
+
