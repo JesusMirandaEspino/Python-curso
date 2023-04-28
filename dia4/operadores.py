cb3080928a45754b77ab1615d32d5847796fce2c
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-28 09:57:36 -0500

    operadores comparacion parte 4

--- dia4/operadores.py
+++ dia4/operadores.py
@@ -39,3 +39,20 @@ print(bool2)
 
 bool2 = not 10 == 10
 print(bool2)
+
+num1 = 36
+num2 = 72/2
+num3 = 48
+mi_bool = num3 >  num1 > num2
+
+
+num1 = 36
+num2 = 72/2
+num3 = 48
+mi_bool =   num1 > num2 or num3 > num1
+
+frase = "Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan"
+palabra1 = "éxito"
+palabra2 = "tecnología"
+
+mi_bool = not palabra1 in frase and not palabra2 in frase
