150b61edd64f3ad4b7be880a812f1b8d1980b67d
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-04-28 09:30:32 -0500

    operadores comparacion parte 1

---
+++ dia4/operadores.py
@@ -0,0 +1,11 @@
+comparacion = 24 == 45
+print(comparacion)
+
+comparacion = 5 + 3 == 4 * 6
+print(comparacion)
+
+comparacion = 'blanco' == 'Blanco'
+print(comparacion)
+
+comparacion = 'blanco' != 'Blanco'
+print(comparacion)
