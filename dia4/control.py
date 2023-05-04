6863db775eda98e142f502c710d52ec44fb0fe28
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-05-04 09:44:17 -0500

    control parte 1

---
+++ dia4/control.py
@@ -0,0 +1,19 @@
+if 10 > 9: 
+    print('Es correcto')
+    
+if 5 == 2:
+    print('Es correcto')
+else:
+    print('Es Incorrecto')
+    
+    
+mascota = 'Gato'
+
+if mascota == 'Gato':
+    print('Tienes un Gato')
+elif mascota == 'Perro':
+    print('Tienes un Perro')
+elif mascota == 'Pez':
+    print('Tienes un Pez')
+else:
+    print('No se que animal Tienes')
