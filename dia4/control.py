b3923e90466fb125604cb9b0a98e2fccd1adacfc
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-05-04 09:54:11 -0500

    control parte 2

--- dia4/control.py
+++ dia4/control.py
@@ -17,3 +17,27 @@ elif mascota == 'Pez':
     print('Tienes un Pez')
 else:
     print('No se que animal Tienes')
+    
+
+edad = 16
+calificacion = 9
+if edad < 18:
+    print('Es menor de edad')
+    if calificacion > 5:
+        print('Aprobo la Materia')
+    else:
+        print('Reprobo la Materia')
+else:
+    print('Es mayor de edad')
+    
+    
+    
+num1 = input("Ingresa un número:")
+num2 = input("Ingresa otro número:")
+
+if num1 > num2:
+    print(f"{num1} es mayor que {num2}")
+elif num2 > num1:
+    print(f"{num2} es mayor que {num1}")
+else:
+    print(f"{num1} y {num2} son iguales")
