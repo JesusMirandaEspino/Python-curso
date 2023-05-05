a80d4dbfa27e38058961bb76a6b1c201b261d996
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-05-05 10:07:55 -0500

    ciclos parte 4

--- dia4/ciclos.py
+++ dia4/ciclos.py
@@ -39,3 +39,13 @@ lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
 suma_numeros = 0
 for numero in lista_numeros:
     suma_numeros  = suma_numeros + numero
+    
+    
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+suma_pares = 0
+suma_impares = 0
+for numero in lista_numeros:
+    if numero % 2 == 0:
+        suma_pares = suma_pares + numero
+    if numero % 2 == 1:
+        suma_impares = suma_impares + numero
