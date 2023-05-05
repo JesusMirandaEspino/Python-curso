872608022b3e4ef07aa58f7902d2a57350cf3f8a
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-05-05 10:04:27 -0500

    ciclos parte 3

--- dia4/ciclos.py
+++ dia4/ciclos.py
@@ -24,7 +24,18 @@ for letra in palabra:
     print(letra)
     
     
-combinacion = [ [1,2], [[3,4]], [[5,6]] ]
-for a, b in combinacion:
+combinacion = [ [1,2], [3,4], [5,6] ]
+for a,b in combinacion:
     print(a)
     print(b)
+    
+    
+alumnos_clase = ["María", "José", "Carlos", "Martina", "Isabel", "Tomás", "Daniela"]
+for alumno in alumnos_clase:
+    print(f'Hola {alumno}')
+    
+    
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+suma_numeros = 0
+for numero in lista_numeros:
+    suma_numeros  = suma_numeros + numero
