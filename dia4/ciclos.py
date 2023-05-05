fd40a6cfd9fa74c7d8b4accf8f640e43aca5b4ca
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-05-05 10:00:26 -0500

    ciclos parte 2

--- dia4/ciclos.py
+++ dia4/ciclos.py
@@ -10,3 +10,21 @@ nombres = [ 'Pablo', 'Luis', 'Pedro', 'Raul', 'Jose' ]
 for nombre in nombres:
     if nombre.startswith('L'):
         print(nombre)
+        
+        
+numeros = [ 1,2,3,4,5,6,7,8,9 ]
+valor = 0
+for numero in numeros:
+    valor = valor + numero
+    print(valor)
+    
+    
+palabra = 'phyton'
+for letra in palabra:
+    print(letra)
+    
+    
+combinacion = [ [1,2], [[3,4]], [[5,6]] ]
+for a, b in combinacion:
+    print(a)
+    print(b)
